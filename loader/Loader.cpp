bd8ddefbca547a37b3f925624f227dd0eb7cf8ad
Pierre-Hugues Husson <phh@phh.me>
2025-06-16 16:24:02 -0400

    Additional bpf-less handling
    Change-Id: I889334a28d8d484cef9d9e8a8e0ea0aa2c0b47ac

--- loader/Loader.cpp
+++ loader/Loader.cpp
@@ -977,7 +977,8 @@ void initLogging() {
 void createBpfFsSubDirectories() {
     for (const auto& location : android::bpf::locations) {
         if (android::bpf::createSysFsBpfSubDir(location.prefix)) {
-            exit(120);
+            ALOGE("=== CRITICIL FAILURE creating BPF subdir %s", location.prefix);
+            //exit(120);
         }
     }
 }
